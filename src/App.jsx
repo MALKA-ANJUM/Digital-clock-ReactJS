88fe3804fcabd7f6e1e6720d833c5695d48a3b30
Malka <malkaanjum1999@gmail.com>
2022-08-23 23:19:49 +0530

    main code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+    let time = new Date().toLocaleTimeString()
+    const [currentTime, setTime] = useState(time)
+
+    const updateTime = () => {
+        time = new Date().toLocaleTimeString()
+        setTime(time)
+    }
+    setInterval(updateTime, 1000)
   return (
-    <div>
-      
+    <div className='container'>
+      <h1>{currentTime}</h1>
     </div>
   )
 }
